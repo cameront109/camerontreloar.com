737b815428cf96a0107bca3e822e35e8a29f1a28
cameront109 <58799209+cameront109@users.noreply.github.com>
2026-06-09 11:37:01 +0900

    Update slideshow.js

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,1 +1,32 @@
+const slides = [
+    "images/slide01.jpg",
+    "images/slide02.jpg",
+    "images/slide03.jpg",
+    "images/slide04.jpg",
+    "images/slide05.jpg"
+];
 
+let current = 0;
+
+const image = document.getElementById("slideshow");
+
+function nextSlide() {
+
+    image.style.opacity = 0;
+
+    setTimeout(() => {
+
+        current++;
+
+        if (current >= slides.length) {
+            current = 0;
+        }
+
+        image.src = slides[current];
+
+        image.style.opacity = 1;
+
+    }, 500);
+}
+
+setInterval(nextSlide, 4000);
